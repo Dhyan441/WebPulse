8456d5e5a51c91a4a0e22b1a4dec9e97944af535
dhyan441 <dxxxx441@gmail.com>
2024-12-23 18:29:49 -0700

    400 error handeling

--- app/services/speedMonitoring.py
+++ app/services/speedMonitoring.py
@@ -1,8 +1,33 @@
+from selenium import webdriver
 
+
+def selectBrowser(browser: str):
+     
+     if browser.lower() == "chrome":
+         return webdriver.Chrome()
+     elif browser.lower() == "edge":
+        return webdriver.Edge()
+     elif browser.lower() == "firefox":
+         return webdriver.Firefox()
+     elif browser.lower() == "safari":
+         return webdriver.Safari()
+     else:
+        raise ValueError(f"Unsupported browser: {browser}. Please choose from 'chrome', 'edge', 'firefox', 'safari' or '' for a deafult browser.")
 
 async def get_ttfb(url: str, browser: str):
 
-    print("Success")
+    # if the browser method is not defined just use the simple method
+    try:
+        if(browser == ""):
+            print("default ttfb")
+
+
+        driver = selectBrowser(browser)
+
+        print("Success")
+
+    except ValueError as e:
+        raise ValueError(e)
 
 async def get_page_size(url: str, browser: str):
 
